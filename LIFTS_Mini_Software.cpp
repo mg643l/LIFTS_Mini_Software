ad7a41f8cf983e7dc712c58ce1cec47a3453511a
Joseph Wood <845jwood@gmail.com>
2026-08-27 18:41:56 +0100

    Working blinky

--- LIFTS_Mini_Software.cpp
+++ LIFTS_Mini_Software.cpp
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+constexpr uint LED_PIN = 6;
 
 int main()
 {
     stdio_init_all();
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        gpio_put(LED_PIN, 1);
+        printf("LED on\n");
+        sleep_ms(500);
+        gpio_put(LED_PIN, 0);
+        printf("LED off\n");
+        sleep_ms(500);
     }
 }
